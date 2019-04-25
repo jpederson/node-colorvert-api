3645821449c699898c06e64524bd43e1cda5c27c
James Pederson <james@jpederson.com>
2019-04-25 11:18:22 -0500

    Add gruntfile back in.

---
+++ gruntfile.js
@@ -0,0 +1,75 @@
+
+
+module.exports = function(grunt) {
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+        // watch for changes and trigger compass, jshint, uglify and livereload
+        watch: {
+            js: {
+                files: ['public/js/src/*.js'],
+                tasks: ['uglify'],
+                options: {
+                    livereload: true,
+                },
+            },
+            css: {
+                files: 'public/css/src/*.scss',
+                tasks: ['sass'],
+                options: {
+                    livereload: true,
+                },
+            }
+        },
+
+        // we use the Sass
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'public/css/main.css': 'public/css/src/main.scss'
+                }
+            }
+        },
+
+        // uglify to concat & minify
+        uglify: {
+            js: {
+                files: {
+                    'public/js/main.js': [
+                        'public/js/lib/jquery.min.js',
+                        'public/js/src/main.js'
+                    ]
+                }
+            }
+        },
+
+        // bump package.json for publishing
+        bump: {
+            options: {
+                pushTo: 'origin'
+            }
+        },
+
+        // concurrent blocking task management
+        concurrent: {
+            target: {
+                tasks: [ 'watch' ],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        }
+
+    });
+
+
+    // register task
+    grunt.registerTask('default', ['concurrent']);
+};
+
