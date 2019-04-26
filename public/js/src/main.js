58d6cc986664d8b98c23d35c32015e982165fd1d
James Pederson <james@jpederson.com>
2019-04-26 00:39:18 -0500

    Comment the front-end javascript so it's clear what we're doing. Version for release.

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -2,18 +2,23 @@
 
 $(function(){
 	
+	// get the individual value of a particular 
 	var get_part_value = function( part ) {
 		return $( "#"+part ).val();
 	},
 
+
+	// convert colors
 	get_colors = function(){
 
 		// get the selected color space
 		var space = $("#space").val(),
 			endpoint;
 
+		// depending on the current color space, build our color conversion endpoint urls.
 		switch ( space ) {
 
+			// deal with cmyk conversions
 			case "cmyk":
 				endpoint = "/cmyk/" + get_part_value( "cmyk-c" ) +
 					"/" + get_part_value( "cmyk-m" ) +
@@ -21,34 +26,40 @@ $(function(){
 					"/" + get_part_value( "cmyk-k" );
 			break;
 
+			// deal with hex conversions
 			case "hex":
 				endpoint = "/hex/" + get_part_value( "hex" );
 			break;
 
+			// deal with hsl conversions
 			case "hsl":
 				endpoint = "/hsl/" + get_part_value( "hsl-h" ) +
 					"/" + get_part_value( "hsl-s" ) +
 					"/" + get_part_value( "hsl-l" );
 			break;
 
+			// deal with hsv conversions
 			case "hsv":
 				endpoint = "/hsv/" + get_part_value( "hsv-h" ) +
 					"/" + get_part_value( "hsv-s" ) +
 					"/" + get_part_value( "hsv-v" );
 			break;
 
+			// deal with lab conversions
 			case "lab":
 				endpoint = "/lab/" + get_part_value( "lab-l" ) +
 					"/" + get_part_value( "lab-a" ) +
 					"/" + get_part_value( "lab-b" );
 			break;
 
+			// deal with rgb conversions
 			case "rgb":
 				endpoint = "/rgb/" + get_part_value( "rgb-r" ) +
 					"/" + get_part_value( "rgb-g" ) +
 					"/" + get_part_value( "rgb-b" );
 			break;
 
+			// deal with xyz conversions
 			case "xyz":
 				endpoint = "/xyz/" + get_part_value( "xyz-x" ) +
 					"/" + get_part_value( "xyz-y" ) +
@@ -57,8 +68,10 @@ $(function(){
 
 		}
 
+		// get the results for the current set of colors.
 		$.getJSON( endpoint, function( response ){
 
+			// manipulate the URL before we output it in the response div.
 			var url = window.location + endpoint.substring( 1 );
 
 			// set it into the response div
@@ -72,6 +85,8 @@ $(function(){
 
 	};
 
+
+	// when they select a color space, change the fields
 	$("#space").change(function(){
 
 		// hide visible fieldsets
@@ -82,17 +97,27 @@ $(function(){
 
 	});
 
+
+	// when they click the convert button, get the results
 	$("#convert").click( get_colors );
 
+
+	// on keyup, if they press enter, get the results
 	$("input[type=text]").keyup( function( e ){
 		if ( e.keyCode === 13 ) {
 			get_colors();
 		}
 	});
 
+
+	// show/hide more info section on click
 	$(".more-info").click(function(){
 		$(".more").slideToggle( 700 );
 	});
+
+
+	// include the clipboard library so we can copy converted values to the clipboard and use them elsewhere.
+	new ClipboardJS('.clipboard');
 
 });
 
